9a60a43c3eff77a387378ec0ccecf24876270e22
Michał Krzyżanowski <krzyzak@users.noreply.github.com>
2025-09-19 15:11:17 +0200

    rubocop

--- app/helpers/turbo_streams/game_helper.rb
+++ app/helpers/turbo_streams/game_helper.rb
@@ -1,20 +1,22 @@
 # frozen_string_literal: true
 
-module TurboStreams::GameHelper
-  def move_pet(params)
-    turbo_stream_action_tag :move_pet, target: :pet, **params
-  end
+module TurboStreams
+  module GameHelper
+    def move_pet(params)
+      turbo_stream_action_tag :move_pet, target: :pet, **params
+    end
 
-  def turbo_redirect(params)
-    turbo_stream_action_tag :turbo_redirect, **params
-  end
+    def turbo_redirect(params)
+      turbo_stream_action_tag :turbo_redirect, **params
+    end
 
-  def increase_points(params)
-    turbo_stream_action_tag :increase_points, **params
-  end
+    def increase_points(params)
+      turbo_stream_action_tag :increase_points, **params
+    end
 
-  def delayed_remove(params)
-    turbo_stream_action_tag :delayed_remove, **params
+    def delayed_remove(params)
+      turbo_stream_action_tag :delayed_remove, **params
+    end
   end
 end
 
