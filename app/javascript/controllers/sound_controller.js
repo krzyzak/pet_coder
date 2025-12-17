f2bb7096e3f5ab12a044882b086503817fc0fe4d
jerryskye <jarek@siedlarz.com>
2025-12-17 16:03:24 +0100

    Add the easier sound effects

--- app/javascript/controllers/sound_controller.js
+++ app/javascript/controllers/sound_controller.js
@@ -1,9 +1,25 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["backgroundMusic"];
+  static targets = ["backgroundMusic", "gameOver", "gameCompleted"];
 
   playBackgroundMusic() {
-    this.backgroundMusicTarget.play();
+    if (this.hasBackgroundMusicTarget) {
+      this.backgroundMusicTarget.play().catch(e => console.error("Could not play background music", e));
+    }
+  }
+
+  gameOverTargetConnected(element) {
+    if (!element.dataset.played) {
+      element.play().catch(e => console.error("Could not play game over sound", e));
+      element.dataset.played = true;
+    }
+  }
+
+  gameCompletedTargetConnected(element) {
+    if (!element.dataset.played) {
+      element.play().catch(e => console.error("Could not play game completed sound", e));
+      element.dataset.played = true;
+    }
   }
 }
